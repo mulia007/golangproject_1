69a41c579f8cfca36053b738ac74fcea657e7b94
M Yusuf <muliayusuf007@gmail.com>
2023-07-25 03:16:44 +0700

    add task11

--- 11_Connection and Interaction DB and ORM (Gorm)/main.go
+++ 11_Connection and Interaction DB and ORM (Gorm)/main.go
@@ -1,138 +1,179 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/labstack/echo/v4"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo"
 )
 
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	InitDB()
+	InitialMigration()
+}
+
+type Config struct {
+	DB_Username string
+	DB_Password string
+	DB_Port     string
+	DB_Host     string
+	DB_Name     string
+}
+
+func InitDB() {
+
+	config := Config{
+		DB_Username: "root",
+		DB_Password: "root123",
+		DB_Port:     "3306",
+		DB_Host:     "localhost",
+		DB_Name:     "crud_go",
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
+	)
+
+	var err error
+	DB, err = gorm.Open("mysql", connectionString)
+	if err != nil {
+		panic(err)
+	}
+}
+
 type User struct {
-	Id       int    `json:"id" form:"id"`
+	gorm.Model
 	Name     string `json:"name" form:"name"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
 }
 
-var users []User
+func InitialMigration() {
+	DB.AutoMigrate(&User{})
+}
 
+// get all users
 func GetUsersController(c echo.Context) error {
-	if users == nil {
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"messages": "users empty",
-		})
+	var users []User
+
+	if err := DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"messages": "success get all users",
-		"users":    users,
+		"message": "success get all users",
+		"users":   users,
 	})
 }
 
+// get user by id
 func GetUserController(c echo.Context) error {
-	var id, _ = strconv.Atoi(c.Param("id"))
+	idParam := c.Param("id")
+	id := parseIdParam(idParam)
+
 	var user User
-	var isFound bool
-	for _, v := range users {
-		if v.Id == id {
-			user = v
-			isFound = true
+	if err := DB.First(&user, id).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "user not found",
+			})
 		}
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-
-	if isFound {
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"messages": "success get user detail",
-			"user":     user,
-		})
-	}
-
-	return c.JSON(http.StatusNotFound, map[string]interface{}{
-		"messages": "user not found",
-	})
-}
-
-func DeleteUserController(c echo.Context) error {
-	var id, _ = strconv.Atoi(c.Param("id"))
-	var tempUser []User
-
-	for _, v := range users {
-		if v.Id != id {
-			tempUser = append(tempUser, v)
-		}
-	}
-
-	if len(tempUser) == len(users) {
-		return c.JSON(http.StatusNotFound, map[string]interface{}{
-			"messages": "user not found",
-		})
-	}
-
-	users = tempUser
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"messages": "success delete user",
-		"users":    users,
+		"message": "success get user by id",
+		"user":    user,
 	})
 }
 
-func UpdateUserController(c echo.Context) error {
-	var id, _ = strconv.Atoi(c.Param("id"))
-	var isFound bool
-	var tempUser []User
-	var user = User{}
-	c.Bind(&user)
-
-	for _, v := range users {
-		if v.Id == id {
-			v = user
-			v.Id = id
-			tempUser = append(tempUser, v)
-			isFound = true
-		} else {
-			tempUser = append(tempUser, v)
-		}
-	}
-
-	users = tempUser
-
-	if isFound {
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"messages": "success update user",
-			"user":     tempUser,
-		})
-	}
-
-	return c.JSON(http.StatusNotFound, map[string]interface{}{
-		"messages": "user not found",
-	})
-}
-
+// create new user
 func CreateUserController(c echo.Context) error {
 	user := User{}
 	c.Bind(&user)
 
-	if len(users) == 0 {
-		user.Id = 1
-	} else {
-		newId := users[len(users)-1].Id + 1
-		user.Id = newId
+	if err := DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	users = append(users, user)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	idParam := c.Param("id")
+	id := parseIdParam(idParam)
+
+	var user User
+	if err := DB.First(&user, id).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "user not found",
+			})
+		}
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := DB.Delete(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"messages": "success create user",
-		"user":     user,
+		"message": "success delete user by id",
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	idParam := c.Param("id")
+	id := parseIdParam(idParam)
+
+	var user User
+	if err := DB.First(&user, id).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "user not found",
+			})
+		}
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	// binding data
+	if err := c.Bind(&user); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success update user by id",
+		"user":    user,
 	})
 }
 
 func main() {
+	// create a new echo instance
 	e := echo.New()
-
+	// Route / to handler function
 	e.GET("/users", GetUsersController)
 	e.GET("/users/:id", GetUserController)
 	e.POST("/users", CreateUserController)
 	e.DELETE("/users/:id", DeleteUserController)
 	e.PUT("/users/:id", UpdateUserController)
 
+	// start the server, and log if it fails
 	e.Logger.Fatal(e.Start(":8000"))
 }
